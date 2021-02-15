1517d27b92f8b15e2d6ec16fb75c1f4d838e7a3c
unknown <fegoworks@hotmail.com>
2021-02-15 15:34:09 +0100

    feat(routes) - set up routes

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import env from 'dotenv';
-import userRoute from './routes/user.route';
+import routes from './routes/index';
+import { apiErrorHandler } from './helpers/errorHandler';
 
 env.config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5555;
 const app = express();
 
 app.use((req, res, next) => {
@@ -32,43 +33,23 @@ app.use(
 app.use(cookieParser());
 
 // Routes here
-app.use('/api/v1/', userRoute);
+app.use(routes);
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Welcome to the ----- Application</h1>
-  <h4>Please use PostMan and navigate to <code>/api/v1</code> to use the app</h4>
-  <p>For any more info please visit my <a href=''>Github</a> page</P>
+  res.send(`<h1>Welcome to the Pub/Sub Application</h1>
+  <h4>Please use PostMan to explore the APIs</h4>
+  <p>For any more info please visit my <a href='https://github.com/fegoworks/HTTP-notification-system'>Github</a> page</P>
   <h4>Thanks  &#x1F600;</h4>`);
 });
 
 app.all('*', (req, res) => {
   res.status(404).json({
     status: 'error',
-    message: 'you have entered an incorrect route',
+    message: 'You have entered an incorrect route',
   });
 });
+app.use(apiErrorHandler);
 
-app
-  .listen(port, () => console.log(`Welcome, listening on ${port}`))
-  .on('error', (err) => {
-    if (err.syscall !== 'listen') {
-      throw err;
-    }
-    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-    // handle specific listen errors with friendly messages
-
-    switch (err.code) {
-      case 'EACCES':
-        console.error(`${bind} requires elevated privileges`);
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(`${bind} is already in use`);
-        process.exit(1);
-        break;
-      default:
-        throw err;
-    }
-  });
+app.listen(port, () => console.log(`Welcome, listening on ${port}`));
 
 export default app;
